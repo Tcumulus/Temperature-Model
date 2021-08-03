f406ab91ff09c9223d831a823c0c8412d7c44d1a
Maarten-Laureyssen <maarten.laureyssen@gmail.com>
2021-08-03 20:32:34 +0200

    Final Data

--- Code/Data-Filter/second_filter.py
+++ Code/Data-Filter/second_filter.py
@@ -1,11 +1,11 @@
 import csv
 
 def append_list_as_row(list_of_elem):
-    with open(f"/Users/Maarten/Documents/Projects/Temperature-Model/Data/Final_data/{list_of_elem[0]}_{list_of_elem[1]}.txt", 'a+', newline='') as write_obj:
+    with open(f"/Users/Maarten/Documents/Projects/Temperature-Model/Data/Final_data/{int(list_of_elem[0])}_{int(list_of_elem[1])}.txt", 'a+', newline='') as write_obj:
         csv_writer = csv.writer(write_obj)
         csv_writer.writerow(list_of_elem)
 
-with open("/Users/Maarten/Documents/Projects/Temperature-Model/Data/Final_Data.txt") as file:
+with open("/Users/Maarten/Documents/Projects/Temperature-Model/Data/Filtered_data/Combined_filtered_data.txt") as file:
     print("TEST")
     reader = csv.reader(file, delimiter=',')
     [append_list_as_row(row) for row in reader]
