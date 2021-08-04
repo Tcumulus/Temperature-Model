487591d015f4a90d256479c2886ca58a07175873
Maarten-Laureyssen <maarten.laureyssen@gmail.com>
2021-08-04 15:16:14 +0200

    Linear model 1 (no bias correction)

--- Code/Model-Training/linear_model_1.py
+++ Code/Model-Training/linear_model_1.py
@@ -19,42 +19,23 @@ y = np.array(data[predict])
 x = np.array(x, dtype=float)
 y = np.array(y, dtype=float)
 
-best, best_b = 100000, 0
-total_best, total_b = 100000, 0
-best_l = []
-def train(b):
+linear = linear_model.LinearRegression()
+
+best = 0
+def train():
+    global linear
+    global best
+
     x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1) # Train and test data chunks
-    linear = linear_model.LinearRegression()
     linear = linear.fit(x_train, y_train)
 
-    y_test = list(y_test)
-    x_pred = list()
-    for row in x_test:
-        predict = float(list(linear.predict([row]))[0])
-        x_pred.append(predict * b)
+    accuracy = linear.score(x_test, y_test)
+    print(accuracy)
 
-    acc_l = list()
-    for n in range(len(x_pred)):
-        acc_l.append(abs(x_pred[n] - y_test[n]))
-    accuracy = sum(acc_l) / len(acc_l)
-
-    global best
-    global best_b
     if accuracy < best:
         best = accuracy
-        best_b = b
 
-def b_loop():
-    global best, best_b, total_best, total_b, best_l, best_b_l
-    best, best_b = 100000, 0
-    [train((b+1)/10) for b in range(50)] #Train
+[train() for _ in range(25)]
 
-    best_l.append(best_b)
-    if best < total_best:
-        total_best = best
-        total_b = best_b
-    print(best, best_b)
-
-[b_loop() for _ in range(25)]
-
-print(f" -- {total_best} -- {total_b} -- {sum(best_l) / len(best_l)}")
+print(f" -- {round(best, 2)} -- ")
+print(f"{round(float(linear.predict([[0,0,0]]))/10, 1)}°C")
