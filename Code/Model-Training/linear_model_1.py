0ae6567ede279b2638b3b35e00be1cd59711c867
Maarten-Laureyssen <maarten.laureyssen@gmail.com>
2021-08-04 14:56:08 +0200

    Added bias reformer

--- Code/Model-Training/linear_model_1.py
+++ Code/Model-Training/linear_model_1.py
@@ -1,3 +1,4 @@
+from numpy.lib.function_base import average
 import pandas as pd
 import numpy as np
 from sklearn import linear_model
@@ -18,13 +19,42 @@ y = np.array(data[predict])
 x = np.array(x, dtype=float)
 y = np.array(y, dtype=float)
 
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1) # Train and test data chunks
-linear = linear_model.LinearRegression()
-linear = linear.fit(x_train, y_train)
-y_pred = linear.predict(x_test)
+best, best_b = 100000, 0
+total_best, total_b = 100000, 0
+best_l = []
+def train(b):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.1) # Train and test data chunks
+    linear = linear_model.LinearRegression()
+    linear = linear.fit(x_train, y_train)
 
-accuracy = linear.score(x_test, y_test)
-print(accuracy)
+    y_test = list(y_test)
+    x_pred = list()
+    for row in x_test:
+        predict = float(list(linear.predict([row]))[0])
+        x_pred.append(predict * b)
 
-predictions = linear.predict([[0, 0, 0]])
-print(predictions)
+    acc_l = list()
+    for n in range(len(x_pred)):
+        acc_l.append(abs(x_pred[n] - y_test[n]))
+    accuracy = sum(acc_l) / len(acc_l)
+
+    global best
+    global best_b
+    if accuracy < best:
+        best = accuracy
+        best_b = b
+
+def b_loop():
+    global best, best_b, total_best, total_b, best_l, best_b_l
+    best, best_b = 100000, 0
+    [train((b+1)/10) for b in range(50)] #Train
+
+    best_l.append(best_b)
+    if best < total_best:
+        total_best = best
+        total_b = best_b
+    print(best, best_b)
+
+[b_loop() for _ in range(25)]
+
+print(f" -- {total_best} -- {total_b} -- {sum(best_l) / len(best_l)}")
